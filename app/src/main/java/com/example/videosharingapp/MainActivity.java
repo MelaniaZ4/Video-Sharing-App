eac11fd3697178e5c25723af1ce7572d46cc1c00
Melanie Brandes <77643560+MelaniaZ4@users.noreply.github.com>
2024-06-06 10:15:24 +1000

    Implement main activity screen.

--- app/src/main/java/com/example/videosharingapp/MainActivity.java
+++ app/src/main/java/com/example/videosharingapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.videosharingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnWatchVideo = findViewById(R.id.watchVideoBtn);
+        Button btnListChannel = findViewById(R.id.listChannelBtn);
+
+        btnWatchVideo.setOnClickListener(view -> watchVideoFunction());
+
+        btnListChannel.setOnClickListener(view -> listChannelFunction());
+    }
+
+    private void listChannelFunction() {
+        // Navigate to YouTubeChannelListActivity
+    }
+
+    private void watchVideoFunction() {
+        //navigate to YouTubeVideoActivity
     }
 }
